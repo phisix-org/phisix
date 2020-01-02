f43eb68bd7c711c64232aa1a20e139494c1f0cda
Edge Dalmacio <edgar.dalmacio@gmail.com>
2020-01-03 03:56:55 +0800

    deployment prep

--- appengine-update.sh
+++ appengine-update.sh
@@ -1,4 +1,4 @@
-mvn clean package appengine:deploy -Pphisix-4
-mvn clean package appengine:deploy -Pphisix-3
-mvn clean package appengine:deploy -Pphisix-2
+# mvn clean package appengine:deploy -Pphisix-4
+# mvn clean package appengine:deploy -Pphisix-3
+# mvn clean package appengine:deploy -Pphisix-2
 mvn clean package appengine:deploy
